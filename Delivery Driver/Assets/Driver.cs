057fe30a8ff2ae77bb7227512ded70e8abdc8019
Vigneshraja K <vigneshrajaks@gmail.com>
2022-10-31 10:20:01 +0530

    Added car object

---
+++ Delivery Driver/Assets/Driver.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+
+    [SerializeField] float steerSpeed = 1f;
+    [SerializeField] float moveSpeed = 0.01f;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        // transform.Rotate(0, 0, 45);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+        if(Input.GetAxis("Vertical") != 0)
+        {
+            transform.Rotate(0, 0, -steerAmount);
+        }
+        transform.Translate(0, moveAmount, 0);
+    }
+}
